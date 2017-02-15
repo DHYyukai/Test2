b97dff8f8dd0ba1fef6ba6cca6ee8001e50fcceb
yukai <358456147@qq.com>
2017-02-15 11:00:01 +0800

    test2

--- app/src/main/java/com/example/nis/test/MainActivity.java
+++ app/src/main/java/com/example/nis/test/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.nis.test;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView=getWindow().getDecorView();
+        int option=View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(option);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
     }
 }
